77f9c563e8ce3727d9b8fda9ae171a0b258901c4
nicklong98 <work@singularity-id.com>
2020-06-07 18:55:45 -0400

    real initial commit

---
+++ Quote.Commands/Commands.fs
@@ -0,0 +1,37 @@
+﻿module QuoteBot.Commands
+
+type Quote = Quote of string with
+    member __.asString =
+        match __ with
+        | Quote q -> q
+        
+type Username = Username of string with
+    member __.asString =
+        match __ with
+        | Username u -> u 
+
+type Command =
+    | AddQuote of Username*Quote
+    | RetrieveRandomQuote
+    | RetrieveRandomQuoteFromUser of Username
+
+let tryParseCommandAndArgs commandIndicator (inputStr:string) =
+    if inputStr.StartsWith commandIndicator then
+        let parts = inputStr.Split(' ')
+        let commandName = parts.[0].ToLower()
+        match commandName with
+        | "stash" ->
+            let username = parts.[1] |> Username
+            let quoteStartIndex =
+                (username.asString |> inputStr.IndexOf) + username.asString.Length + 1
+            let quote = inputStr.Substring quoteStartIndex |> Quote
+            (username, quote) |> AddQuote |> Some
+        | "get" ->
+            if parts.Length < 2 then RetrieveRandomQuote |> Some
+            else
+                let username = parts.[1] |> Username
+                username |> RetrieveRandomQuoteFromUser |> Some
+        | _ -> None
+    else
+        None
+        
---
+++ Quote.DataModel/DataModel.fs
@@ -0,0 +1,26 @@
+﻿namespace Quote.DataModel
+
+open System
+
+type UserId = private UserId of int with
+    static member ofInt i = i |> UserId
+    member __.asInt = match __ with UserId u -> u
+    static member zero = 0 |> UserId
+    
+type QuoteId = private QuoteId of int with
+    static member ofInt i = i |> QuoteId
+    member __.asInt = match __ with QuoteId q -> q
+    static member zero = 0 |> QuoteId
+
+
+type User = {
+    Id: UserId
+    Name: string
+}
+
+type Quote = {
+    Id: QuoteId
+    UserId: UserId
+    Quote: string
+    AddedAt: DateTime
+}
